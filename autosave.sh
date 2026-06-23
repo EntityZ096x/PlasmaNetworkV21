ced2b52b281cae099484b1b647417635f455f85c
EntityZ096x <entity-z096x@hotmail.com>
2026-06-23 06:39:34 +0000

    Hourly backup 2026-06-23 06:39:34

--- autosave.sh
+++ autosave.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+echo 'autosaving every hour'
 while true; do
     git add .
-    git commit -m "Daily backup $(date '+%Y-%m-%d %H:%M:%S')" || true
+    git commit -m "Hourly backup $(date '+%Y-%m-%d %H:%M:%S')" || true
     git push
-    sleep 86400
+    sleep 3600
 done
